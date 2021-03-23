dac65563787a4c6b15ecd0a84fcbc87da5c65efa
Koen van Leeuwen <koen@logiqs.nl>
2021-03-23 08:54:17 +0100

    Tests for sorting bits, needed for From

---
+++ BitPrefixTrieTests/BitsSortingTests.cs
@@ -0,0 +1,32 @@
+﻿using BitPrefixTrie;
+using Xunit;
+
+namespace BitPrefixTrieTests
+{
+    public class BitsSortingTests
+    {
+        public static object[][] LeftIsSmallerThanRight = new[]
+        {
+            new object[] {new Bits(false), new Bits(true)},
+            new object[] {new Bits(false, true), new Bits(true)},
+            new object[] {new Bits(false, true), new Bits(false, true, true)},
+            new object[] {new Bits(false, true), new Bits(false, true, false)},
+        };
+
+        [Theory]
+        [MemberData(nameof(LeftIsSmallerThanRight))]
+        public void Left_is_smaller_than_right(Bits left, Bits right)
+        {
+            Assert.True(left < right);
+            Assert.False(right < left);
+        }
+
+        [Theory]
+        [MemberData(nameof(LeftIsSmallerThanRight))]
+        public void Right_is_greater_than_left(Bits left, Bits right)
+        {
+            Assert.False(left > right);
+            Assert.True(right > left);
+        }
+    }
+}
