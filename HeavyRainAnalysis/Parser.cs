760c96a509d659464d5c3990257635f68c65eade
Koen van Leeuwen Logiqs B.V <koen@logiqs.nl>
2021-07-19 10:11:07 +0200

    reduce allocations

--- HeavyRainAnalysis/Parser.cs
+++ HeavyRainAnalysis/Parser.cs
@@ -2,80 +2,88 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using Microsoft.VisualBasic.FileIO;
+using System.Text;
 
 namespace HeavyRainAnalysis
 {
     public class Parser
     {
-        private readonly int[] _fieldWiths = new[] { 21, 20, 48, 20, 20, 20, 20, 20, 20, 20, 20 };
+        private readonly int[] _fieldWidths = new[] { 19, 22, 48, 20, 20, 20, 20, 20, 20, 20, 20 };
         readonly string _timestampFormat = "yyyy-MM-dd HH:mm:ss";
         private readonly string _format;
+        private readonly int[] _offsets;
 
         public Parser()
         {
-            _format = $"{{0,-{_fieldWiths[0]}:{_timestampFormat}}}" +
-                      $"{{1,-{_fieldWiths[1]}}}" +
-                      $"{{2,-{_fieldWiths[2]}}}" +
-                      $"{{3,-{_fieldWiths[3]}:##.000000}}" +
-                      $"{{4,-{_fieldWiths[4]}:##.000000}}" +
-                      $"{{5,-{_fieldWiths[5]}}}" +
-                      $"{{6,-{_fieldWiths[6]}}}" +
-                      $"{{7,-{_fieldWiths[7]}}}" +
-                      $"{{8,-{_fieldWiths[8]}}}" +
-                      $"{{9,-{_fieldWiths[9]}}}" +
-                      $"{{10,-{_fieldWiths[10]}}}";
+            _format = $"{{0,-{_fieldWidths[0]}:{_timestampFormat}}}" +
+                      $"{{1,-{_fieldWidths[1]}}}" +
+                      $"{{2,-{_fieldWidths[2]}}}" +
+                      $"{{3,-{_fieldWidths[3]}:##.000000}}" +
+                      $"{{4,-{_fieldWidths[4]}:##.000000}}" +
+                      $"{{5,-{_fieldWidths[5]}}}" +
+                      $"{{6,-{_fieldWidths[6]}}}" +
+                      $"{{7,-{_fieldWidths[7]}}}" +
+                      $"{{8,-{_fieldWidths[8]}}}" +
+                      $"{{9,-{_fieldWidths[9]}}}" +
+                      $"{{10,-{_fieldWidths[10]}}}";
+            _offsets = new int[_fieldWidths.Length];
+            var offset = 0;
+            for (int i = 0; i < _fieldWidths.Length; i++)
+            {
+                _offsets[i] = offset;
+                offset += _fieldWidths[i];
+            }
         }
 
         public IEnumerable<RainData> Parse(Stream contents)
         {
-            var parser = new TextFieldParser(contents);
-            parser.CommentTokens = new[] { "#" };
-            parser.TextFieldType = FieldType.FixedWidth;
-            parser.SetFieldWidths(_fieldWiths);
-            parser.TrimWhiteSpace = true;
-            while (!parser.EndOfData)
+            var textReader = new StreamReader(contents, Encoding.ASCII);
+            var line = textReader.ReadLine();
+            while (line != null)
             {
-                var fields = parser.ReadFields();
-                if (fields == null) yield break;
-                // DTG Date/time stamp
-                //# LOCATION = identifier, NAME = identifier, LATITUDE in degrees (WGS84), LONGITUDE in degrees (WGS84), ALTITUDE in 0.1 m relative to Mean Sea Level (MSL)
-                //# DR_PWS_10 is neerslag duur present weather sensor 10' eenheid seconde
-                //# DR_REGENM_10 is neerslag duur electrische regenmeter 10' eenheid seconde
-                //# WW_COR_10 is weer gecorr. code present weather sensor 10' eenheid code
-                //# RI_PWS_10 is neerslag intensiteit present weather sensor 10' eenheid millimeter per uur
-                //# RI_REGENM_10 is neerslag intensiteit electrische regenmeter 10' eenheid millimeter per uur
-                int field = 0;
-                yield return new RainData
+                if (!line.StartsWith("#"))
                 {
-                    Aggregation = TimeSpan.FromMinutes(10),
-                    Timestamp = DateTime.ParseExact(fields[field++], _timestampFormat, CultureInfo.InvariantCulture),
-                    Id = fields[field++],
-                    Name = fields[field++],
-                    Latitude = ParseDouble(fields[field++]),
-                    Longitude = ParseDouble(fields[field++]),
-                    Altitude = ParseDouble(fields[field++]),
-                    DurationSensor = ParseTimeSpan(fields[field++]),
-                    DurationGauge = ParseTimeSpan(fields[field++]),
-                    WCor = fields[field++],
-                    IntensitySensor = ParseDouble(fields[field++]),
-                    IntensityGauge = ParseDouble(fields[field++]),
-                };
+                    yield return Parse(line);
+                }
+                line = textReader.ReadLine();
             }
         }
 
-        private TimeSpan ParseTimeSpan(string input)
+        private RainData Parse(string line)
         {
-            if (string.IsNullOrEmpty(input))
-                return TimeSpan.Zero;
-            return TimeSpan.FromSeconds(float.Parse(input, CultureInfo.InvariantCulture));
+            ReadOnlySpan<char> GetSpan(int index) =>
+             line.AsSpan(_offsets[index], _fieldWidths[index]);
+
+            int field = 0;
+            return new RainData
+            {
+                Aggregation = TimeSpan.FromMinutes(10),
+                Timestamp = DateTime.ParseExact(GetSpan(field++), _timestampFormat, CultureInfo.InvariantCulture),
+                Id = new string(GetSpan(field++)),
+                Name = new string(GetSpan(field++)),
+                Latitude = ParseDouble(GetSpan(field++)),
+                Longitude = ParseDouble(GetSpan(field++)),
+                Altitude = ParseDouble(GetSpan(field++)),
+                DurationSensor = ParseTimeSpan(GetSpan(field++)),
+                DurationGauge = ParseTimeSpan(GetSpan(field++)),
+                WCor = new string(GetSpan(field++)),
+                IntensitySensor = ParseDouble(GetSpan(field++)),
+                IntensityGauge = ParseDouble(GetSpan(field++)),
+            };
         }
 
-        private double ParseDouble(string input)
+        private TimeSpan ParseTimeSpan(ReadOnlySpan<char> input)
         {
-            if (string.IsNullOrEmpty(input))
-                return 0.0;
-            return double.Parse(input, CultureInfo.InvariantCulture);
+            if (float.TryParse(input, NumberStyles.Number, CultureInfo.InvariantCulture, out var seconds))
+                return TimeSpan.FromSeconds(seconds);
+            return TimeSpan.Zero;
+        }
+
+        private double ParseDouble(ReadOnlySpan<char> input)
+        {
+            if (double.TryParse(input, NumberStyles.Number, CultureInfo.InvariantCulture, out var result))
+                return result;
+            return 0.0;
         }
 
         public string Serialize(RainData input)
